a8e25c7ad3c4534f9805e5cb2f8208aa1a6a505e
Вячеслав Бегишев <vb@MacBook-Air-Vaceslav.local>
2022-11-02 15:51:46 +0300

    Add task#08

---
+++ Task08/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Введите целое число:  ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// // string str = "4"; 
+
+// int square = number * number; 
+// Console.WriteLine($"Квадрат числа {number} = {square}");
+
+Console.WriteLine("Введите целое число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 1; 
+
+while (count <= number)
+{
+    if (count %2 == 0)
+    {
+        Console.Write($"{count}   ");
+    }
+    count++;
+} 
