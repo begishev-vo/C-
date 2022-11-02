44ad6d55ac353dae977c04d5d5e84060ab56e436
Вячеслав Бегишев <vb@MacBook-Air-Vaceslav.local>
2022-11-02 15:19:03 +0300

    Add task06

---
+++ Task06/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два 
+// без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("Введите целое число:  ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// // string str = "4"; 
+
+// int square = number * number; 
+// Console.WriteLine($"Квадрат числа {number} = {square}");
+
+Console.WriteLine("Введите целое число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
