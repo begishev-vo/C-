891bb90c1d1b0c81b7828b6b1454c29cebb0b6be
vobegishev <vobegishev@gmail.com>
2022-12-15 09:27:18 +0300

    Add Task

---
+++ Task66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Console.Write("Введите число M: ");
+// int numberM = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число N: ");
+// int numberN = Convert.ToInt32(Console.ReadLine());
+
+int SumNatM_N(int M, int N)
+{
+    int st = M;
+    int finish = N;
+    if (M > N)
+    {
+        st = N;
+        finish = M;
+    }
+    return (finish + st)*(finish - st +1)/2; 
+}
+
+int sum_m_n = SumNatM_N(8, 2);
+Console.WriteLine($"Сумма чисел:  {sum_m_n}");
---
+++ Task68/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+static uint AkkNumber(unit M, unit N)
+{
+    if (M == 0)
+        return N + 1;
+    else 
+      if ( (N != 0) && (M == 0))
+        return AkkNumber(M -1, 1);
+    else
+        return AkkNumber(M - 1, AkkNumber(M, N-1)); 
+}
+
+Console.WriteLine(AkkNumber(1, 2));
+Console.WriteLine(AkkNumber(0, 0));
+
+Console.ReadKey();
